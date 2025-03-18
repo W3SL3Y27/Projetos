3c3567fb66ffef683cecfde5455c76eff89613e4
W3SL3Y27 <wesley.fsjs27@gmail.com>
2025-03-18 17:24:20 -0300

    Update aula2.py

--- aula2.py
+++ aula2.py
@@ -1,6 +1,8 @@
 import random
 quer_continuar = True
 contador = 1
+acertos = 0
+erros = 0
 while(quer_continuar):
     n1 = random.randint(1,1000)
     n2 = random.randint(1,1000)
@@ -10,3 +12,9 @@ while(quer_continuar):
         print("resposta correta")
     else:
         print("resposta incorreta")
+    contador = contador+1
+    continuar = input("Quer continuar? [1]sim [2nao]")
+    if(continuar ==2):
+        quer_continuar = False
+
+print("FIM")

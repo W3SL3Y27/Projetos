30985a29b3fb06f04e1ac0aa6f35ec5ac9059223
W3SL3Y27 <wesley.fsjs27@gmail.com>
2025-03-24 11:09:44 -0300

    Add files via upload

---
+++ prova02.py
@@ -0,0 +1,15 @@
+prova1 = int(input("nota prova1: "))
+prova2 = int(input("nota prova2: "))
+nota = prova1+prova2
+
+if(nota/2 >=5):
+    print("aprovado")
+else:
+    print("reprovado")
+    print("direito a uma prova de recuperaçao")
+    prov3 = int(input("nota recuperaçao: "))
+nota+=prov3
+if(nota/2 >=5):
+    print("aprovado")
+else:
+    print("reprovado")
---
+++ prova02test.py
@@ -0,0 +1,14 @@
+prova1 = int(input("nota prova1: "))
+prova2 = int(input("nota prova2: "))
+nota = prova1+prova2
+
+if(nota/2 >=5):
+    print("aprovado")
+elif print("reprovado"):
+    print("direito a uma prova de recuperaçao")
+    prov3 = int(input("nota recuperaçao: "))
+nota+=prov3
+if(nota/2 >=5):
+    print("aprovado")
+else:
+    print("reprovado")

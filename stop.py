5aaec22366eea729de5ccf5e045377fc7f6ae1bc
W3SL3Y27 <wesley.fsjs27@gmail.com>
2025-04-29 20:39:23 -0300

    Update stop.py

--- stop.py
+++ stop.py
@@ -1,4 +1,43 @@
+p = ["Andrade",]
 e = input("digite uma letra: ")
 
 if e == "a":
-    print("andrade")
+    print(p[0])
+elif e == "b":
+    print(p[1])
+elif e == "c":
+    print("CS GO")
+elif e == "d":
+    print("Dragon ball")
+elif e == "e":
+    print("Enderson")
+elif e == "f":
+    print("Fortnite")
+elif e == "g":
+    print("GTA")
+elif e == "h":
+    print("helicoptero")
+elif e == "i":
+    print("informatica")
+elif e == "j":
+    print("jujutsu kaisen")
+elif e == "k":
+    print("kiwi")
+elif e == "l":
+    print("LOL")
+elif e == "m":
+    print("Minecraft")
+elif e == "n":
+    print("NBA")
+elif e == "o":
+    print("One Piece")
+elif e == "p":
+    print("Erik")
+elif e == "q":
+    print("Erik")
+elif e == "r":
+    print("Erik")
+elif e == "s":
+    print("Erik")
+elif e == "t":
+    print("Erik")
